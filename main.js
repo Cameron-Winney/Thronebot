6a0acea09cf23f449cc4f7191b9369a71091787b
Cameron Winney <coolcam1111@gmail.com>
2020-07-28 06:51:11 -0500

    forgot to redo the help command

--- main.js
+++ main.js
@@ -26,19 +26,8 @@ client.on('message', message => {
 
 
     //general commands
-    if (message.content === `${prefix}help`) {
-        message.channel.send(`
-    >>help -- gives you list of commands.
-    >>ping -- Pong!
-    >>user-info -- gives you anyone's real username and discord ID.
-    >>members -- lists how many people are in the server.
-    >>roll _ -- you can put a number after and it will roll a die for you. By default it will roll 6 sided die.
-    >>multi-dice -- similar to >>roll but you can roll multiple dice.
-    >>coin -- I will flip a coin for you.
-    >>magic-8-ball -- you can ask a question and I will give you an answer.
-    THESE NEXT ONES ARE ADMIN ONLY!
-    >>kick -- kicks specified user.
-    >>ban -- bans specified user.`)
+    if (command === 'help') {
+        client.commands.get('help').execute(message, args);
     }
 
     if (command === `ping`) {
@@ -70,10 +59,10 @@ client.on('message', message => {
         if (command === `kick`) {
             client.commands.get('kick').execute(message, args);
         }
-        if (message.member.hasPermission("BAN_MEMBERS")) {
-            if(command === `ban`){
-            client.commands.get('ban').execute(message, args);
-            }
+    }
+    if (message.member.hasPermission("BAN_MEMBERS")) {
+        if(command === `ban`){
+        client.commands.get('ban').execute(message, args);
         }
     }
 
