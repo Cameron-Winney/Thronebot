6777da0d3d9c79f48500a550a29ecf0ffd432bae
Cameron Winney <coolcam1111@gmail.com>
2020-08-14 15:22:56 -0500

    basic command layout

---
+++ commands/template.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'Name', //command name
+    description: 'Info',
+    dev: true,
+    args: true,
+    cooldown: 1,
+	execute(message, args) {
+        //sample text
+	},
+};
