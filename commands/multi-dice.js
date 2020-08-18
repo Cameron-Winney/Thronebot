77430b67459df8d18a9d040185b7550a5e6027fb
Thorn <stachelrodtr4@gmail.com>
2020-08-17 22:42:17 -0600

    multi-dice logic and format fix

--- commands/multi-dice.js
+++ commands/multi-dice.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js')
 module.exports = {
     name: "multi-dice",
     description: "Roll multiple dice. Each number will be parsed as a unique die.",
@@ -6,18 +7,23 @@ module.exports = {
     cooldown: 10,
     usage: "[arg1] [arg2] ... [argX]",
     execute(message, args) {
-        let diceArray = []
-        let diceObj = {}
+        const client = message.guild.members.client
+        const diceArray = []
+        const sidesArray = []
         if (args.length < 2) { return message.channel.send("Multi Dice only works with 2 or more dice. If you wanted one die, use >>roll.") }
 
         for (let i = 0; i < args.length; i++) {
-            diceArray.push(args[i].match(/\d+/))
+            let number = args[i].match(/\d+/)
+            sidesArray.push(number)
+            diceArray.push(Math.floor(Math.random() * number) + 1)
         }
-        for (let j = 0; j < diceArray.length; j++) {
-            diceObj[j + 1] = Math.floor(Math.random() * diceArray[j]) + 1
-        }
-        let diceString = JSON.stringify(diceObj)
-        message.channel.send(`The results are: `)
-        message.channel.send(diceString.substring(1, (diceString.length - 1)).split("\"").join(""))
+        const diceEmbed = new Discord.MessageEmbed()
+            .setTitle("Dice Results")
+            .setFooter("This post brought to you by NerdBot", client.user.displayAvatarURL())
+            for (let j=0; j<diceArray.length; j++){
+                diceEmbed.addField(`Die ${j+1}, ${sidesArray[j]} sides`, diceArray[j], true)
+            }
+        message.channel.send(diceEmbed)
+        
     }
 }
